9850c8f764c341d49ec98d75e681e0f5f36d4117
regireed89 <regireed89@gmail.com>
2018-01-18 12:06:55 -0600

    letter gathering

--- Parser.py
+++ Parser.py
@@ -4,12 +4,15 @@ class Parser(object):
         self.expression = expression
         self.grammar = [' ', '*', '(', ')', '+']
         self.clauses = self.expression.replace('*', ' ')
-        
-               
+        for e in self.expression:
+            if e in self.grammar:
+                self.characters = self.expression.replace(e, ' ')
+            
+
 def main():
     parser = Parser('(A + B) * (B + C)')
     print parser.clauses
-    print parser.grammar
+    print parser.characters
 
 
         
