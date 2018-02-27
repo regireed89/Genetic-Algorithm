626e718eeef4bd744d2d16ec423307cd476aeb1b
regireed89 <regireed89@gmail.com>
2018-02-26 20:38:24 -0600

    chromosomes

---
+++ Chromosome.py
@@ -0,0 +1,22 @@
+class Chromosome(object):
+
+    def __init__(self):
+        self.genes = ""
+        self.fitness = 0
+
+    def Fitness(self):
+        num1 = int(self.genes[0]) + int(self.genes[1])
+        num2 = int(self.genes[2]) + int(self.genes[3])
+        num3 = int(self.genes[4]) + int(self.genes[5])
+        num4 = int(self.genes[6]) + int(self.genes[7])
+        self.fitness = (num1 + num2) - (num3 + num4)
+        return self.fitness
+            
+def main():
+    '''Main'''
+    c = Chromosome()
+    c.genes = "11001180"
+    c.Fitness()
+    print(c.fitness)
+
+main()
